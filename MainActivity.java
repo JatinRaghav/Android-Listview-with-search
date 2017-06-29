9defe2f8d741bee4f0d9f9588e0ff61d5658d721
JatinRaghav <Jatin.raghav@nuevothoughts.com>
2017-06-29 15:21:06 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,70 +1,128 @@
-package com.example.androidlistviewwithsearch;
- 
+package nuevothoughts.com.githublistview;
+
+
 import java.util.ArrayList;
 import java.util.HashMap;
- 
+import java.util.Locale;
+
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
- 
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class MainActivity extends Activity {
-     
+
     // List view
     private ListView lv;
-     
+
     // Listview Adapter
     ArrayAdapter<String> adapter;
-     
+
     // Search EditText
     EditText inputSearch;
-     
-     
+    private ImageButton btnSpeak;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+
     // ArrayList for Listview
     ArrayList<HashMap<String, String>> productList;
- 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         
+
         // Listview Data
-        String products[] = {"Dell Inspiron", "HTC One X", "HTC Wildfire S", "HTC Sense", "HTC Sensation XE",
-                                "iPhone 4S", "Samsung Galaxy Note 800",
-                                "Samsung Galaxy S3", "MacBook Air", "Mac Mini", "MacBook Pro"};
-         
+        String products[] = {"Apple", "Blackberry","Blackcurrant","Banana", "Dates","Gooseberry","Grapes","Guava", "Mangosteen",
+                "Mango","Orange","Papaya","Plum","Peach","Pineapple","Pomogranate","Sapota","Sweetlime","Watermelon","Zizyphus"};
+
         lv = (ListView) findViewById(R.id.list_view);
         inputSearch = (EditText) findViewById(R.id.inputSearch);
-         
+        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+        // hide the action bar
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
+
         // Adding items to listview
         adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.product_name, products);
         lv.setAdapter(adapter);
-         
+
         /**
          * Enabling Search Filter
          * */
         inputSearch.addTextChangedListener(new TextWatcher() {
-             
+
             @Override
             public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
                 // When user changed the Text
-                MainActivity.this.adapter.getFilter().filter(cs);   
+                MainActivity.this.adapter.getFilter().filter(cs);
             }
-             
+
             @Override
             public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
-                    int arg3) {
+                                          int arg3) {
                 // TODO Auto-generated method stub
-                 
+
             }
-             
+
             @Override
             public void afterTextChanged(Editable arg0) {
-                // TODO Auto-generated method stub                          
+                // TODO Auto-generated method stub
             }
         });
-    }    
+    }
+
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    inputSearch.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
 }
